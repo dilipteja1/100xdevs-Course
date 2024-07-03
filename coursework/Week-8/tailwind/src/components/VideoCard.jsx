32439c2aadcfa7655d7a22a2bec2df70b232b1aa
dilipteja1 <tejadilip23898@gmail.com>
2024-07-03 17:47:13 -0400

    paytm backend initial

---
+++ coursework/Week-8/tailwind/src/components/VideoCard.jsx
@@ -0,0 +1,18 @@
+
+export function VideoCard(){
+    return  <div>
+        hi there
+        {/* video */}
+        <img src="/100xdevs-Course/coursework/Week-8/tailwind/public/thumbnail.svg" />
+        {/* Name of the video */}
+        <div></div>
+        {/* Name of the channel */}
+        <div></div>
+        {/* Views and time */}
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+
+    </div>
+}
