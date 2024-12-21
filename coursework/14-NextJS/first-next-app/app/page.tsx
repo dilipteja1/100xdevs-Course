e7a2e906f7b99910e6d943315554a332e212464f
dilipteja1 <tejadilip23898@gmail.com>
2024-12-21 13:02:28 -0500

    Introduce Loading in  Next

--- coursework/14-NextJS/first-next-app/app/page.tsx
+++ coursework/14-NextJS/first-next-app/app/page.tsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
+import axios from "axios";
+import { use } from "react";
 
-export default function Home() {
+async function getUserData() {
+  await new Promise((r) => setTimeout(r, 5000))
+  const response = axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return (await response).data
+} 
+ export default async function Home() {
+  const userDetails = await getUserData();
   return (
-   <div>Hello world</div>
+   <div>Hello world
+    {userDetails?.email}
+    {userDetails?.name}
+
+   </div>
+   
   );
 }
